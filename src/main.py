fe3e6b1622c37d5fd1995223a552517ee709dfc7
Christo <hah51@mail.aub.edu>
2022-09-28 16:18:59 -0400

    add the models

--- src/main.py
+++ src/main.py
@@ -1,11 +1,19 @@
 import pandas as pd
-
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import f_classif
+from matplotlib import pyplot
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import svm
 
 def get_data():
     file_name = './data/drug_consumption.data'
     # Read the file in pandas data frame
     data = pd.read_csv(file_name, header=None)
-    # store the dataset
+    # store the datasfrom sklearn.metrics import accuracy_scoreet
     dataset = data.values
     return dataset
 
@@ -34,7 +42,7 @@ print(type(data[0][0]))
 
 # I will split the data into data and labels
 X = data[:, :-1]
-label = data[:, -1]
+labels = data[:, -1]
 
 def convert_to_number(data):
     m, n = data.shape
@@ -57,9 +65,59 @@ def convert_to_number(data):
                     data[i][j] = 6
     return data
 
+def prepare_targets(y_train, y_test):
+	le = LabelEncoder()
+	le.fit(y_train)
+	y_train_enc = le.transform(y_train)
+	y_test_enc = le.transform(y_test)
+	return y_train_enc, y_test_enc
+
 
 print(X[30, :])
 X = convert_to_number(X)
 print(X[30, :])
 print(X.shape)
-print(label)
+print(labels)
+
+X_train, X_test, y_train, y_test = train_test_split(X, labels, test_size=0.33, random_state=1)
+y_train_enc, y_test_enc = prepare_targets(y_train, y_test)
+from sklearn.metrics import accuracy_score
+print(y_train)
+print(y_train_enc)
+
+fs = SelectKBest(score_func=f_classif, k=16)
+fs.fit(X_train, y_train)
+X_train_fs = fs.transform(X_train)
+X_test_fs = fs.transform(X_test)
+
+for i in range(len(fs.scores_)):
+	print('Feature %d: %f' % (i, fs.scores_[i]))
+# plot the scores
+pyplot.bar([i for i in range(len(fs.scores_))], fs.scores_)
+pyplot.show()
+
+print(X_train_fs.shape)
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(X_train, y_train)
+
+X_result = clf.predict(X_test)
+#print(X_result)
+print(accuracy_score(y_test, X_result))
+
+# random forest
+clf = RandomForestClassifier(random_state=0)
+clf.fit(X_train, y_train)
+X_result = clf.predict(X_test)
+print(accuracy_score(y_test, X_result))
+
+clf = svm.SVC()
+clf.fit(X_train, y_train)
+X_result = clf.predict(X_test)
+print(accuracy_score(y_test, X_result))
+
+
+from sklearn.neighbors import KNeighborsClassifier
+clf = KNeighborsClassifier(n_neighbors=5)
+clf.fit(X_train, y_train)
+X_result = clf.predict(X_test)
+print(accuracy_score(y_test, X_result))
